e30ab6be0262f02939702045d62fcf56feceb703
Jolo Ortiz <jolo.ortiz@gmail.com>
2012-06-25 18:48:46 +0800

    Revert "FIRST COMMIT - CTS"
    This reverts commit 41a27f779b4f0af14b67e032b573ccb2df6340e9.

--- application/models/referrals_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Referrals_model extends CI_Model {
-
-	function get_refers(){
-
-		$query = $this->db->get('referers');
-		return $query->result();
-	}
-}
--- application/views/themes/master/footer_view.php
+++
@@ -1,6 +0,0 @@
-</div> <!-- #content end -->
-<footer>
- 	<p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
-</footer>
-</body>
-</html>
