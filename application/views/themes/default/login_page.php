5b89abba9256046b0c2ae82417b9808bf0dbca50
Jolo Ortiz <jolo.ortiz@gmail.com>
2012-06-26 11:28:27 +0800

    Bernie Changes - Login Page and CSS edits

--- application/views/themes/default/login_page.php
+++ application/views/themes/default/login_page.php
@@ -10,6 +10,6 @@
 				<?php echo form_input( $attrib_username ); ?>
 				<?php echo form_password( $attrib_password ); ?>
 				<?php echo form_submit( $attrib_submit ); ?>
-				<?php echo form_label( form_checkbox( $attrib_remember_me ) . 'keep me logged in!' ,'remember_me' ,$attrib_remember_label ); ?>
+				<?php echo form_label( form_checkbox( $attrib_remember_me ) . 'Keep me logged in!' ,'remember_me' ,$attrib_remember_label ); ?>
 		<?php echo form_close(); ?>
-<p class="aligncenter">forgot your password? <a href="">Click Here</a></p>
+<p class="aligncenter">Forgot your password? <a href="">Click Here</a></p>
